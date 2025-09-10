df75c7d3ed1150192992d6d9282fb9a21b106ed0
Otavio Astrath <otavioastrath00@gmail.com>
2025-09-10 11:08:53 -0300

    created chat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_09_182400) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_10_134048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_09_182400) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_messages_on_event_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -62,4 +72,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_09_182400) do
   add_foreign_key "attendances", "users"
   add_foreign_key "events", "matches"
   add_foreign_key "events", "users"
+  add_foreign_key "messages", "events"
+  add_foreign_key "messages", "users"
 end
