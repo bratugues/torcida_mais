1b06d97261fbec65ccbc96df2b0d6fc00e2e53d0
Agata Martinez <agatapetramali@gmail.com>
2025-09-11 12:41:55 -0300

    Atualiza schema com novas migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_10_192828) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_11_151139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,6 +90,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_10_192828) do
     t.string "location"
     t.string "team"
     t.boolean "bar"
+    t.string "bar_name"
+    t.string "bar_address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
