1a14b91d896217012dc7f5ce813d968d3a95bf16
Agata Martinez <agatapetramali@gmail.com>
2025-09-11 15:58:09 -0300

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -120,7 +120,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_11_170913) do
     t.string "location"
     t.string "team"
     t.boolean "bar"
-    t.string "bar_name"
     t.string "bar_address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
