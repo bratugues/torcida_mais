e1c956738ba91647fd855a60a2e00ead7bbb1cd4
Erick Lobo <erickloboabreu@hotmail.com>
2025-09-13 11:54:02 -0300

    fix forms with f.association instead of f.input collection for events and signup

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :match
+  belongs_to :club
   has_many :reviews, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :attendances, dependent: :destroy
 
-  validates :name, :address, :team, :description, :date, presence: true
+  validates :name, :address, :club, :description, :date, presence: true
 
   def average_rating
     reviews.average(:rating)
