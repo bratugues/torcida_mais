a0068a0c743ab0d3aa2b92780dd02f3cb88d60c2
Erick Lobo <erickloboabreu@hotmail.com>
2025-09-13 21:17:29 -0300

    add club shields to the events cards and show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,12 @@ class PagesController < ApplicationController
 
   def dashboard
     my_club = current_user.club
-    @my_matches = Match.where("home_team_id = :club_id OR away_team_id = :club_id", club_id: my_club.id)
+    
+    if my_club
+      @my_matches = Match.where("home_team_id = :club_id OR away_team_id = :club_id", club_id: my_club.id)
+    else
+      @my_matches = Match.none
+    end
 
     @other_matches = Match.where("played_at >= ?", Time.current)
                           .order(:played_at)
