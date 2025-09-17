cd5856b1dea08bfe1969585126a04fa1f43e9f8c
Erick Lobo <erickloboabreu@hotmail.com>
2025-09-17 12:17:33 -0300

    added place field to edit form

--- app/javascript/controllers/sweetalert_controller.js
+++ app/javascript/controllers/sweetalert_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
   text: "You won't be able to revert this!",
   icon: "warning",
   showCancelButton: true,
-  confirmButtonColor: "#3085d6",
+  confirmButtonColor: "#386150",
   cancelButtonColor: "#d33",
   confirmButtonText: "Yes, delete it!"
   }).then((result) => {
